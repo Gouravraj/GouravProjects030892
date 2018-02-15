83f0def9539289f61152d16f03c18f5ab0821410
Gouravraj <kumargourav030892@gmail.com>
2018-02-14 23:08:35 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,93 @@
+var express = require("express");
+
+var bodyparser=require("body-parser");
+
+var app=express();
+
+var mongoose=require("mongoose");
+
+mongoose.connect('mongodb://localhost/emp');
+
+var Employee = mongoose.model('Employee',mongoose.Schema({
+
+	name:String,
+	dept:String,
+	status:String,
+	salary:String,
+	contact:String
+
+
+}));
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+//app.use(express.static(_dirname + '/client'));
+
+app.get('/api/employees',function(req,res){
+
+	Employee.find(function(err,employees) {
+		// body...
+		if(err)
+				res.send(err);
+			res.json(employees);
+	});
+});
+
+
+app.get('/api/employees/:id',function(req,res){
+
+	Employee.findOne({_id:req.params.id},function(err,employees) {
+		
+		if(err)
+				res.send(err);
+			res.json(employees);
+	});
+});
+
+app.post('/api/employees',function(req,res){
+
+	Employee.create(req.body,function(err,employees) {
+		
+		if(err)
+				res.send(err);
+			res.json(employees);
+	});
+});
+app.delete('/api/employees/:id',function(req,res){
+
+	Employee.findOneAndRemove({_id:req.params.id},function(err,employees) {
+		
+		if(err)
+				res.send(err);
+			res.json(employees);
+	});
+});
+app.delete('/api/employees/:id',function(req,res){
+
+	Employee.findOneAndUpdate ({_id:req.params.id},function(err,employees) {
+		
+		if(err)
+				res.send(err);
+			res.json(employees);
+	});
+});
+
+app.put('/api/employees/:id',function(req,res){
+	var query = {
+		name:req.body.name,
+		dept:req.body.dept,
+		status:req.body.status,
+		salary:req.body.salary,
+		contact:req.body.contact
+};
+	Employee.findOneAndUpdate({_id:req.params.id},query,function(err,employees) {
+		
+		if(err)
+				res.send(err);
+			res.json(employees);
+	});
+});
+app.listen(8000,function(){
+
+	console.log("server is running at port 8000");
+});	
